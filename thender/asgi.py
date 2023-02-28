7cba5ef47a7a8b52a7e026fbe66d3d5d7b59d3ea
toluhunter <toluhunter19@gmail.com>
2023-02-28 15:15:38 +0100

    Fixed Django setup for daphne

--- thender/asgi.py
+++ thender/asgi.py
@@ -1,15 +1,13 @@
-import os
+from .setup_asgi import *
 
 from django.core.asgi import get_asgi_application
 
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 
 from transmission.router import websocket_urlpatterns
 from .middleware import JWTAuthMiddleWare
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thender.settings')
 
 application = ProtocolTypeRouter(
     {
