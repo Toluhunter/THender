8e79926601090ad703c8e0c5656e2a9ff63633c9
toluhunter <toluhunter19@gmail.com>
2023-03-08 08:49:02 +0100

    Added serving of files

--- thender/urls.py
+++ thender/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 from rest_framework_simplejwt.views import TokenRefreshView
+
 
 from account.views import SearchUserView
 from .views import HealthCheckView
@@ -10,4 +14,4 @@ urlpatterns = [
     path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
     path("health/", HealthCheckView.as_view(), name="health-check"),
     path("peer/", include('peer.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDUA_ROOT)
