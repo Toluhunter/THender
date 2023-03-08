0e1a762ae26725a0d5b0949799ada129d0b5c278
toluhunter <toluhunter19@gmail.com>
2023-03-08 08:39:02 +0100

    Changed health check response

--- thender/views.py
+++ thender/views.py
@@ -5,4 +5,4 @@ from rest_framework.response import Response
 class HealthCheckView(generics.GenericAPIView):
 
     def get(self, request):
-        return Response("Healthy", status=status.HTTP_200_OK)
+        return Response("OK", status=status.HTTP_200_OK)
